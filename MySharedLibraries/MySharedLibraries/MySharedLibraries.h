0b7365f2f053e34e792baef067eb0e436d31f573
Nicholas Servidio <servidionicholas@gmail.com>
2018-01-27 17:22:25 -0500

    added shared library

---
+++ MySharedLibraries/MySharedLibraries/MySharedLibraries.h
@@ -0,0 +1,19 @@
+//
+//  MySharedLibraries.h
+//  MySharedLibraries
+//
+//  Created by Nicholas Servidio on 1/27/18.
+//  Copyright © 2018 Nicholas Servidio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySharedLibraries.
+FOUNDATION_EXPORT double MySharedLibrariesVersionNumber;
+
+//! Project version string for MySharedLibraries.
+FOUNDATION_EXPORT const unsigned char MySharedLibrariesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySharedLibraries/PublicHeader.h>
+
+
